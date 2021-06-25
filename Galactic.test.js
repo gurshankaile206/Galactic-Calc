daaad29a72c368e7cb8bb6b9ea9faea67b098227
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-25 14:01:28 -0700

    add first test

--- Galactic.test.js
+++ Galactic.test.js
@@ -1,12 +1,17 @@
+import {Age} from '../Galactic/src//Galactic.js';
+
+
 class Age {
     constructor()
-  }
+  };
 
   describe ('Age', () => {
 
-  })
+  });
 
 
   test('Age Class should exist', () =>{
       expect(Age).toBeDefined();
-  })
+  });
+
+  
