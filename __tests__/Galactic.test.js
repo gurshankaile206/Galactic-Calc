36183ab7925bc4f86ec2c8a819dcfdfc30dfb725
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-05 22:39:00 -0700

    add tests folder

--- __tests__/Galactic.test.js
+++ __tests__/Galactic.test.js
@@ -1,5 +1,5 @@
 //import Galctic from './src/Galactic.js';
-const Galactic = require('./src/Galactic.js')
+const Galactic = require('../src/Galactic.js')
 
 describe('Galactic', function(){
   let galacticAge;
@@ -9,40 +9,31 @@ describe('Galactic', function(){
     galacticAge = new Galactic(25)
 
   });
-
   test('returns input earth age', function(){
     expect(galacticAge.getEarthAge()).toEqual(25);
   });
-  
   test('returns Mercury Age', function(){
     expect(galacticAge.getMerAge()).toEqual(6);
   });
-
   test('return Venus Age', function(){
     expect(galacticAge.getVenusAge()).toEqual(15)
   });
-
   test('return Mars Age', function(){
     expect(galacticAge.getMarsAge()).toEqual(47)
   });
-
   test('returns Jupiter Age', function(){
     expect(galacticAge.getJptAge()).toEqual(296);
   });
-
   test('returns life expectancy on Mercury', function(){
     expect(galacticAge.lifeExpOnMer()).toEqual(9);
   });
-
   test('return life expectancy on Venus', function(){
     expect(galacticAge.lifeExpOnVenus()).toEqual(24)
   }); 
-
   test('return life expectancy on Mars', function(){
     expect(galacticAge.lifeExpOnMars()).toEqual(75);
   });
-
   test('return life expectancy on Jupiter', function() {
     expect(galacticAge.lifeExpOnJp()).toEqual(474);
-  })
+  });
 });
--- src/Galactic.js
+++ src/Galactic.js
@@ -1,71 +1,75 @@
 class Galactic{
 
-constructor(earthAge) {
-this.earthAge = earthAge
-this.mercuryAge=0; //.24 earth years
-this.venusAge=0; //.62 
-this.marsAge=0;//1.88
-this.jupiterAge=0; //11.86
-this.averageLife = 65;
-}
+  constructor(earthAge) {
+    this.earthAge = earthAge
+    this.mercuryAge=0; //.24 earth years
+    this.venusAge=0; //.62 
+    this.marsAge=0;//1.88
+    this.jupiterAge=0; //11.86
+    this.averageLife = 65;
+  }
 
-getEarthAge() {
-return this.earthAge
-}
+  getEarthAge() {
+    return this.earthAge
+  }
 
-getMerAge() {
-this.mercuryAge = this.earthAge*0.24;
-return Math.floor(this.mercuryAge);
-}
+  getMerAge() {
+    this.mercuryAge = this.earthAge*0.24;
+    return Math.floor(this.mercuryAge);
+  }
 
-getVenusAge() {
-this.venusAge = this.earthAge*0.62;
-return Math.floor(this.venusAge);
-}
+  getVenusAge() {
+    this.venusAge = this.earthAge*0.62;
+    return Math.floor(this.venusAge);
+  }
 
-getMarsAge() {
-this.marsAge = this.earthAge*1.88;
-return Math.floor(this.marsAge);
-}
+  getMarsAge() {
+    this.marsAge = this.earthAge*1.88;
+    return Math.floor(this.marsAge);
+  }
 
-getJptAge() {
-this.jupiterAge = this.earthAge*11.86;
-return Math.floor(this.jupiterAge); 
-}
+  getJptAge() {
+    this.jupiterAge = this.earthAge*11.86;
+    return Math.floor(this.jupiterAge); 
+  }
 
-lifeExpOnMer() {
-if(this.earthAge > this.averageLife) {
-    let surpassedYears = (this.earthAge-this.averageLife)*0.24;
-    return Math.floor(surpassedYears);
-} else { 
-    let remainingYears = this.averageLife - this.earthAge;
-    return Math.floor(remainingYears*0.24);
-}
-}
-
-lifeExpOnVenus() {
+  lifeExpOnMer() {
     if(this.earthAge > this.averageLife) {
-        let surpassedYears = (this.earthAge - this.averageLife)*0.62;
+        let surpassedYears = (this.earthAge - this.averageLife)*0.24;
         return Math.floor(surpassedYears);
     } else {
         let remainingYears = this.averageLife - this.earthAge;
-        return Math.floor(remainingYears*0.62);
+        return Math.floor(remainingYears*0.24);
     }
-}
-
-lifeExpOnMars() {
+  }
+  lifeExpOnVenus() {
+    if(this.earthAge > this.averageLife) {
+        let surpassedYears = (this.earthAge - this.averageLife)*.62;
+        return Math.floor(surpassedYears);
+    } else {
+        let remainingYears = this.averageLife - this.earthAge;
+        return Math.floor(remainingYears*.62);
+    }
+  }
+  lifeExpOnMars() {
     if(this.earthAge > this.averageLife) {
         let surpassedYears = (this.earthAge - this.averageLife)*1.88;
-        return Math.floor(surpassedYears); 
+        return Math.floor(surpassedYears);
     } else {
-        let remainingYears = (this.averageLife - this.earthAge);
+        let remainingYears = this.averageLife - this.earthAge;
         return Math.floor(remainingYears*1.88);
     }
-}
-lifeExpOnJp() {
-    
-}
+  }
+  lifeExpOnJp() {
+    if(this.earthAge > this.averageLife) {
+        let surpassedYearsOnJp = (this.earthAge - this.averageLife)*11.86;
+        return Math.floor(surpassedYearsOnJp);
+    } else {
+        let remainingYears = this.averageLife - this.earthAge;
+        return Math.floor(remainingYears*11.86);
+    }
+  }
 
-}
+};
 
 module.exports = Galactic;
